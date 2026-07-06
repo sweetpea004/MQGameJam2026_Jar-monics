5342aeb6f70d40ca939bb775e582d21c3d65cc44
Matthematicals <126547783+Matthematicals@users.noreply.github.com>
2026-07-06 12:17:39 +1000

    adjust room bounds

--- Jar-monics/Assets/Scripts/Room.cs
+++ Jar-monics/Assets/Scripts/Room.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Room : MonoBehaviour
 {
-private Collider2D room;
+private BoxCollider2D room;
     [SerializeField] private Color colour = Color.blue;
 
     [SerializeField] private Room toLeft;
@@ -35,8 +35,14 @@ public float GetY{
 
 private void OnDrawGizmos(){
     if(room == null){
-room = GetComponent<Collider2D>();
+room = GetComponent<BoxCollider2D>();
     }
+    Camera cam = Camera.main;
+    float aspect = cam.aspect;
+    float yHeight = cam.orthographicSize * 2;
+    // room.bounds = new Bounds(transform.position, new Vector3(aspect * yHeight, yHeight));
+    room.size = new Vector3(aspect * yHeight, yHeight);
+
 
     Gizmos.color = colour;
     Gizmos.DrawWireCube(transform.position, room.bounds.size);
