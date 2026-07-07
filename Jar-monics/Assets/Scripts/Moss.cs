7031863b911d58f83c1ba36703aa65ba89752d54
Ev3rstorm004 <mike.day2@outlook.com>
2026-07-07 19:12:09 +1000

    added sprites to sprite templates

--- Jar-monics/Assets/Scripts/Moss.cs
+++ Jar-monics/Assets/Scripts/Moss.cs
@@ -1,9 +1,8 @@
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class Moss : MonoBehaviour
 {
-    [SerializeField] private Sprite[] stages = new Sprite[3];
+    [SerializeField] private Sprite[] stages = new Sprite[4];
     public Sprite[] Stages
     {
         get
